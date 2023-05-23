f5326ef5ede1c451ffa711ed5df69fe50e309725
Sheer Jay A. Piodos <sheerjay.piodos@cit.edu>
2023-05-24 06:28:06 +0800

    Cart

--- app/src/main/java/com/example/picastrevised/Cart.java
+++ app/src/main/java/com/example/picastrevised/Cart.java
@@ -1,6 +1,7 @@
 package com.example.picastrevised;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -70,6 +72,13 @@ public class Cart extends Fragment {
 
         addDataToList();
 
+        Button btnCheckOut = view.findViewById(R.id.button3);
+        btnCheckOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(requireContext(), Payment.class));
+            }
+        });
         return view;
     }
 
